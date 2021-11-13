fef614ea8745b42b420aa27442a619721a42cb95
ericorabelo <erico.clementee@gmail.com>
2021-11-13 15:13:26 -0300

    add projeto

---
+++ app/src/main/java/erico/rabelo/interfacenetflix/MainActivity.java
@@ -0,0 +1,14 @@
+package erico.rabelo.interfacenetflix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
